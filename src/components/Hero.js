1be69fd347fb33edba579b254622c87fbab05b42
Abraham Mora-Tadeo <132838022+AbrahamM007@users.noreply.github.com>
2025-08-25 18:56:49 -0700

    Updated Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -69,6 +69,7 @@ const MorphingText = ({ text, className }) => {
     <span className={`${className} ${isComplete ? 'complete' : ''}`}>
       {displayText}
       {!isComplete && <span className="cursor-blink">|</span>}
+      }
     </span>
   );
 };
